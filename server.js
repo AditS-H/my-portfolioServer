64ece5cf61880c958c03a2b02191ba953afb1cf2
Adit <aditss2327@gmail.com>
2025-08-31 17:26:31 +0530

    Changes

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || ['http://localhost:3000', 'http://localhost:5173','https://adit-portfolio-psi.vercel.app/'],
+  origin: process.env.FRONTEND_URL || ['http://localhost:3000', 'http://localhost:5173','https://adit-portfolio-psi.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization']
